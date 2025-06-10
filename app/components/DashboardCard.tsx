f6ad85230703c3accac6c56780cd51b610ca3a32
AIYA <33777937+megenius@users.noreply.github.com>
2025-06-10 08:49:05 +0700

    Updated DashboardCard.tsx

--- app/components/DashboardCard.tsx
+++ app/components/DashboardCard.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import { LucideIcon } from 'lucide-react';
+import { DivideIcon as LucideIcon } from 'lucide-react';
 
 interface DashboardCardProps {
   title: string;
